1e31f8e3dc8d400eb79ab86c37adf12ca80dc432
PEREZ <alexperez@wanadoo.fr>
2019-08-22 09:08:43 -0400

    Plotting metaanalysis results

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import extract
 import nimare
+import matplotlib
+import os
+from nilearn import plotting
+from dotenv import load_dotenv
+from matplotlib import pyplot as plt
 
-data_dir = 'data-narps/orig/'  # Data folder
-hyp_file = 'hypo1_unthresh.nii.gz'  # Hypothesis name
+# Set backend for matplotlib
+load_dotenv()
+if 'MPL_BACKEND' in os.environ:
+    matplotlib.use(os.environ['MPL_BACKEND'])
+else:
+    matplotlib.use('TkAgg')
+
 
 if __name__ == '__main__':
+    # Folder and file names
+    data_dir = 'data-narps/orig/'  # Data folder
+    hyp_file = 'hypo1_unthresh.nii.gz'  # Hypothesis name
+
     # Extracting coordinates from data
     ds_dict = extract.extract(data_dir, hyp_file, threshold=.95, load=True)
 
@@ -13,4 +27,13 @@ if __name__ == '__main__':
     ALE = nimare.meta.cbma.ale.ALE()
     res = ALE.fit(ds)
 
-    print(res.maps)
+
+    # Plotting results of meta analysis
+    img_ale = res.get_map('ale')
+    img_p = res.get_map('p')
+    img_z = res.get_map('z')
+
+    plotting.plot_stat_map(img_ale, title='ALE')
+    plotting.plot_stat_map(img_p, title='p')
+    plotting.plot_stat_map(img_z, title='z')
+    plt.show()
