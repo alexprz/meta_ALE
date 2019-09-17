9ffe7fe3d9956d8c0b1cac8928341cfef8861bf0
PEREZ <alexperez@wanadoo.fr>
2019-09-16 22:46:59 -0400

    FDR threshold

--- correct.py
+++ correct.py
@@ -16,6 +16,7 @@ from matplotlib import pyplot as plt
 path_res = os.path.abspath('save/res_meta/KDA_res')
 path_obj = os.path.abspath('save/res_meta/KDA_obj')
 path_t = os.path.abspath('save/res_meta/KDA_t')
+path_p = os.path.abspath('save/res_meta/KDA_p')
 
 with open(path_res, 'rb') as file:
     res = pickle.load(file)
@@ -30,17 +31,22 @@ with open(path_obj, 'rb') as file:
 
 # KDA = nimare.meta.cbma.mkda.KDA()
 # KDA.dat
-arr_p = obj._fwe_correct_permutation(res, n_iters=10000)['logp_level-voxel']
+arr_p = obj._fwe_correct_permutation(res, n_iters=10)['logp_level-voxel']
 
 arr_p = np.exp(-arr_p)
 
+fdr = nimare.stats.fdr(arr_p.ravel(), q=q)
+
+# img_p = nib.Nifti1Image(arr_p, res.get_map('of').affine)
+
 arr_t = res.get_map('of', return_type='array')
 # print(arr_t)
-arr_t[arr_p > 0.05] = 0
+arr_t[arr_p > fdr] = 0
 
 mask = obj.mask
 masker = nimare.utils.get_masker(mask)
 
+img_p = masker.inverse_transform(arr_p)
 img_t = masker.inverse_transform(arr_t)
 # print(arr_t)
 
@@ -50,6 +56,9 @@ plt.show()
 
 with open(path_t, 'wb') as file:
     pickle.dump(img_t, file)
+
+with open(path_p, 'wb') as file:
+    pickle.dump(arr_p, file)
 
 
 ## MKDA
@@ -57,6 +66,7 @@ with open(path_t, 'wb') as file:
 path_res = os.path.abspath('save/res_meta/MKDA_res')
 path_obj = os.path.abspath('save/res_meta/MKDA_obj')
 path_t = os.path.abspath('save/res_meta/MKDA_t')
+path_p = os.path.abspath('save/res_meta/MKDA_p')
 
 with open(path_res, 'rb') as file:
     res = pickle.load(file)
@@ -71,18 +81,21 @@ with open(path_obj, 'rb') as file:
 
 # KDA = nimare.meta.cbma.mkda.KDA()
 # KDA.dat
-arr_p = obj._fwe_correct_permutation(res, n_iters=10000)['logp_level-voxel']
+arr_p = obj._fwe_correct_permutation(res, n_iters=10)['logp_level-voxel']
 
 arr_p = np.exp(-arr_p)
 
+fdr = nimare.stats.fdr(arr_p.ravel(), q=q)
+
 arr_t = res.get_map('of', return_type='array')
 # print(arr_t)
-arr_t[arr_p > 0.05] = 0
+arr_t[arr_p > fdr] = 0
 
 mask = obj.mask
 masker = nimare.utils.get_masker(mask)
 
 img_t = masker.inverse_transform(arr_t)
+img_p = masker.inverse_transform(arr_p)
 # print(arr_t)
 
 # img_t = nib.Nifti1Image(arr_t, res.get_map('of').affine)
@@ -91,3 +104,6 @@ plt.show()
 
 with open(path_t, 'wb') as file:
     pickle.dump(img_t, file)
+
+with open(path_p, 'wb') as file:
+    pickle.dump(img_p, file)
